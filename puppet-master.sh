75e5e5638b9b8f27574539e3707a8cb224d8303c
Shane <shane.pike@outlook.com>
2015-10-09 00:19:05 +0800

    Added error catchers to commands

--- puppet-master.sh
+++ puppet-master.sh
@@ -6,14 +6,16 @@ function install_server() {
 	echo "Installing RHEL 7 Puppet Labs repository"
 	
 	# Install puppet labs repository for RHEL 7 
-	sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+	rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm || error "Failed to add the puppet labs repository"
 
 	# Editing puppet configurations file with alternative dns names. Checks if line already exists. 
 	grep --silent --regexp='dns_alt_name*' $PUPPET_CONF || sed -i "/\[main\]/a\    dns_alt_name = $HOSTNAME" $PUPPET_CONF
 
 	# Creating directories in production environment
-	sudo mkdir --parents /etc/puppet/environments/production/{modules, manifests}
-	sudo touch $ENVIRONMENT_CONF #Create environment configuration file
+	mkdir --parents /etc/puppet/environments/production/{modules, manifests} || error "Failed to make the puppet environment \
+	directories"
+
+	touch $ENVIRONMENT_CONF || error "Could not create environment configuration file" #Create environment configuration file |
 	> $ENVIRONMENT_CONF #Empty file if it already exists
 
 	# Edit configuration file with module path name
@@ -27,18 +29,19 @@ function install_server() {
 
 	# Changing SELinux to Permissive mode to remove any complications
 	echo -n -e "SELinux to permissive mode\n"
-	sudo setenforce permissive
-	sudo sed --in-place 's\=enforcing\=permissive\g' /etc/sysconfig/selinux
+	setenforce permissive || error "Failed to change SELinux to permissive"
+
+	sed --in-place 's\=enforcing\=permissive\g' /etc/sysconfig/selinux || error "Unable to edit the SELinux configuration file"
 
 	# Checks if a certificate exists. If not, generate a new one
         if [ ! -f /var/lib/puppet/ssl/public_keys/$(echo $HOSTNAME | awk '{print tolower($0)}').pem ]; then
 		echo "Generating a certificate now... "
-                sudo puppet master --verbose --no-daemonize
+                 puppet master --verbose --no-daemonize
         else echo -n -e "Certificate already exists, moving on\n"
 	fi
 
 	echo -n -e "Adding TCP Port 8140 to Firewall.\n"
 	#Adding puppet port 8140 to the firewall
-	sudo firewall-cmd --permanent --add-port=8140/tcp
-	sudo firewall-cmd --reload
+	firewall-cmd --permanent --add-port=8140/tcp || error "Failed to add entry to the firewall"
+	firewall-cmd --reload
 	}
